d0b2c818f576bbc681d1115f144231441c48820d
DESKTOP-IPANDAN\chris <chris1889@live.nl>
2018-04-03 15:45:37 +0200

    Checkpoint before refactoring mesh / texture

--- KB01_Engine/ResourceManager.cpp
+++ KB01_Engine/ResourceManager.cpp
@@ -3,6 +3,9 @@
 #include "Renderer.h"
 #include "Log.h"
 
+#include <algorithm>
+
+
 ResourceManager::ResourceManager()
 {
 	textures = std::vector<Texture*>();
@@ -27,28 +30,58 @@ bool ResourceManager::InitResourceLoader(Renderer* _renderer)
 	}
 	return true;
 }
+
 //-----------------------------------------------------------------------------
 // Name: LoadResource(std::string _fileName)
 // Desc: Loads a resource using the filename provided in the parameters
 //-----------------------------------------------------------------------------
 Mesh* ResourceManager::LoadMesh(std::string _filePath, std::string _fileName)
 {
-	Mesh* mesh = meshLoader->LoadResource(_filePath, _fileName);
-	meshes.push_back(mesh);
-	Log::Instance()->LogMessage("ResourceManager - Resource '" + _fileName + "' loaded.", Log::MESSAGE_INFO);
-	return mesh;
+	Mesh* mesh = NULL;
 
+	std::vector<Mesh*>::iterator it = std::find_if(meshes.begin(), meshes.end(), [_fileName](Mesh* temp) { return temp->GetFileName() == _fileName; });
+
+	// if mesh does exist, give it the existing pointer
+	if (it != meshes.end())
+	{
+		mesh = static_cast<Mesh*>(*it);
+
+		Log::Instance()->LogMessage("ResourceManager - Mesh '" + _fileName + "' already loaded.'", Log::MESSAGE_INFO);
+	}
+	// if mesh doesn't exist, load it
+	else 
+	{
+		mesh = meshLoader->LoadResource(_filePath, _fileName);
+		meshes.push_back(mesh);
+
+		Log::Instance()->LogMessage("ResourceManager - Mesh '" + _fileName + "' loaded.", Log::MESSAGE_INFO);
+	}
+	
+	return mesh;
 }
 
 Texture* ResourceManager::LoadTexture(std::string _filePath, std::string _fileName)
 {
-	Texture* texture = textureLoader->LoadResource(_filePath, _fileName);
-	textures.push_back(texture);
-	//if texture contains something
-	if (texture)
+	Texture* texture = NULL;
+	
+	std::vector<Texture*>::iterator it = std::find_if(textures.begin(), textures.end(), [_fileName](Texture* temp) { return temp->GetFileName() == _fileName; });
+
+	// if texture does exist, give it the existing pointer
+	if (it != textures.end())
 	{
+		texture = static_cast<Texture*>(*it);
+
+		Log::Instance()->LogMessage("ResourceManager - Texture '" + _fileName + "' already loaded'", Log::MESSAGE_INFO);
+	}
+	// if texture doesn't exist, load it
+	else
+	{
+		texture = textureLoader->LoadResource(_filePath, _fileName);
+		textures.push_back(texture);
+
 		Log::Instance()->LogMessage("ResourceManager - Texture '" + _fileName + "' loaded.", Log::MESSAGE_INFO);
 	}
+
 
 	return texture;
 }

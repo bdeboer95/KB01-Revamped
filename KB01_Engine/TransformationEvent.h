fbad107d164c742ee3cad1b7331bcb3753521028
B.deBoer95 <b.deboer95@gmail.com>
2018-04-23 17:35:59 +0200

    Added XMLParser and changed TransformationEvent.hpp to .h

---
+++ KB01_Engine/TransformationEvent.h
@@ -0,0 +1,15 @@
+#ifndef __TRANSFORMATIONEVENT_H__
+#define __TRANSFORMATIONEVENT_H__
+
+enum class TRANSFORMATIONEVENT 
+{
+	MOVE_LEFT, //Move to the left
+	MOVE_RIGHT, //Move to the right
+	ROTATE_RIGHT, //Rotate to the right
+	ROTATE_UP, //Rotate upwards
+	ROTATE_LEFT, //Rotate to the left
+	ROTATE_DOWN, //Rotate downwards
+	MOVE_FORWARD, //Move forward
+	MOVE_BACKWARDS, //Move to the back
+};
+#endif
---
+++ KB01_Engine/XMLParser.cpp
@@ -0,0 +1,59 @@
+#include "XMLParser.h"
+#include <fstream>
+
+
+XMLParser::XMLParser()
+{
+}
+
+
+XMLParser::~XMLParser()
+{
+}
+std::string XMLParser::GetFile(const std::string& filename)
+{
+	std::string buffer;
+	char c;
+
+	std::ifstream in(filename);  
+	while (in.get(c)) buffer += c;
+	in.close();
+
+	return buffer;
+}
+
+
+//======================================================================
+
+
+std::vector<std::string> XMLParser::GetData(const std::string& text, const std::string& tag)
+{
+	std::vector<std::string> collection;
+	unsigned int pos = 0, start;
+
+	while (true)
+	{
+		start = text.find("<" + tag, pos);   if (start == std::string::npos) return collection;
+		start = text.find(">", start);
+		start++;
+
+		pos = text.find("</" + tag, start);   if (pos == std::string::npos) return collection;
+		collection.push_back(text.substr(start, pos - start));
+	}
+}
+
+
+//======================================================================
+
+
+void XMLParser::StripTags(std::string& text)
+{
+	unsigned int start = 0, pos;
+
+	while (start < text.size())
+	{
+		start = text.find("<", start);    if (start == std::string::npos) break;
+		pos = text.find(">", start);    if (pos == std::string::npos) break;
+		text.erase(start, pos - start + 1);
+	}
+}
